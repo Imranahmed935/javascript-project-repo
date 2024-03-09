8233bc8d7cdc7bf4be93e3ac8457ff53ab6b3cef
Bestimran <imrantahir9918@gmail.com>
2024-03-10 00:13:38 +0600

    responsive done

--- countDown-timer/js/script.js
+++ countDown-timer/js/script.js
@@ -1,20 +1,20 @@
-const time = "9 march 2024 11:40 pm";
+const time = "10 march 2024 11:59 pm";
 const staticTime = document.getElementById("time");
 staticTime.innerText = time;
 
 const input = document.querySelectorAll("input");
 
 const clock = () => {
-  const endTime = new Date(time);
-  const nowTime = new Date();
-  const diff = (endTime - nowTime) / 1000;
-  if (diff < 0) return;
-  input[0].value = Math.floor(diff / 3600 / 24);
-  input[1].value = Math.floor(diff / 3600) % 24;
-  input[2].value = Math.floor(diff / 60) % 60;
-  input[3].value = Math.floor(diff % 60);
+const endTime = new Date(time);
+const nowTime = new Date();
+const diff = (endTime - nowTime) / 1000;
+if (diff < 0) return;
+input[0].value = Math.floor(diff / 3600 / 24);
+input[1].value = Math.floor(diff / 3600) % 24;
+input[2].value = Math.floor(diff / 60) % 60;
+input[3].value = Math.floor(diff % 60);
 };
 clock();
 setInterval(() => {
-  clock();
+clock();
 }, 1000);
