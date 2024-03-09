f7abb1fc4b46bef4129083357ed6b03fb9cca637
Bestimran <imrantahir9918@gmail.com>
2024-03-10 00:34:33 +0600

    project done

--- countDown-timer/js/script.js
+++ countDown-timer/js/script.js
@@ -5,16 +5,16 @@ staticTime.innerText = time;
 const input = document.querySelectorAll("input");
 
 const clock = () => {
-const endTime = new Date(time);
-const nowTime = new Date();
-const diff = (endTime - nowTime) / 1000;
-if (diff < 0) return;
-input[0].value = Math.floor(diff / 3600 / 24);
-input[1].value = Math.floor(diff / 3600) % 24;
-input[2].value = Math.floor(diff / 60) % 60;
-input[3].value = Math.floor(diff % 60);
+  const endTime = new Date(time);
+  const nowTime = new Date();
+  const diff = (endTime - nowTime) / 1000;
+  if (diff < 0) return;
+  input[0].value = Math.floor(diff / 3600 / 24);
+  input[1].value = Math.floor(diff / 3600) % 24;
+  input[2].value = Math.floor(diff / 60) % 60;
+  input[3].value = Math.floor(diff % 60);
 };
 clock();
 setInterval(() => {
-clock();
+  clock();
 }, 1000);
